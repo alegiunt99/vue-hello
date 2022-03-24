4e1bb1660169b257e0fd00b3bebe7b90d7059ea4
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-24 17:45:06 +0100

    Ho creato il titolo e caricato l'immagine

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 console.log('OK js!')
 
-/*   */
+/* Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.*/
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            testo: 'Sivallettoh',
+
+            image: 'img/momento-gaming.jpg'
+        }
+    }
+);
